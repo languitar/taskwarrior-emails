53f6e00ec0dfe56b232e9752fd22f8f8ffebc1eb
Johannes Wienke <languitar@semipol.de>
2018-08-21 10:47:24 +0200

    Set content enconding

--- send-due-tasks.sh
+++ send-due-tasks.sh
@@ -10,7 +10,7 @@ do
     description=$(echo "$object" | jq -r '.description')
     due=$(echo "$object" | jq -r '.due')
     human_due="$(date -d "$(echo $due | sed -r 's/(.*)T(..)(..)(..)/\1 \2:\3:\4/')")"
-    echo -e "Due task\n\n$description\n\nat: $human_due\nuuid: $uuid" | mail -s "Due task $description" -r tasks@semipol.de languitar@semipol.de -- 
+    echo -e "Due task\n\n$description\n\nat: $human_due\nuuid: $uuid" | mail -a 'Content-Type: text/plain; charset=UTF-8' -s "Due task $description" -r tasks@semipol.de languitar@semipol.de --
     task rc.recurrence.confirmation=no "$uuid" modify +notified
 done
 
